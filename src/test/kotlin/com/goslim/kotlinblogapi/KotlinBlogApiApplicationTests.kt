95ce3c01b4e2df914afeda5d0c74c36760b8f4df
khan <>
2023-03-18 10:48:34 +0900

    first commit

---
+++ src/test/kotlin/com/goslim/kotlinblogapi/KotlinBlogApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.goslim.kotlinblogapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBlogApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
